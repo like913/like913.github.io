21fe35fc1abeac64eb6e9b61f2184c288d774885
like913 <like_913@mail.ru>
2019-09-04 01:03:09 +0600

    Refresh scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,7 +11,7 @@ sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
 sed -i 's/#ru_RU.UTF-8 UTF-8/nu_RU.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
 
-echo -e 'LANG=\"ru_RU.UTF-8\"' > /etc/locale.conf
+echo -e 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
 export LANG=ru_RU.UTF-8
 
 echo 'Настройка времени'
@@ -119,4 +119,3 @@ echo 'Настройка папок пользователя'
 packman -S xdg-user-dirs
 
 exit
-
